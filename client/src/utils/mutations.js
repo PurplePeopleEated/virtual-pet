f2caba49b7c3924052a7318ca648b7f663f164d6
neon <dogmanyo@gmail.com>
2024-02-08 20:04:39 -0500

    added mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,12 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_USER = gql`
-  mutation CreateUser($username: String!, $email: String!) {
-    createUser(username: $username, email: $email) {
+  mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
       _id
       username
       email
+      password
     }
   }
 `;
