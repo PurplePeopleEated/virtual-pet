9d12895d7bfaf5283d5a2d5474ef49ccd1a2955f
Karra <karra.maynard@gmail.com>
2024-02-13 06:43:48 -0500

    init style for pet selection

--- client/src/pages/PetSelection.jsx
+++ client/src/pages/PetSelection.jsx
@@ -51,26 +51,38 @@ const PetSelection = ({ history }) => {
   };
 
   return (
-    <div>
-      <h1>Pet Selection</h1>
-      <div>
-        <label>
-          Choose a pet:
-          <select value={species} onChange={(e) => setSpecies(e.target.value)}>
-            <option value="null"></option>
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="rat">Rat</option>
-          </select>
-        </label>
-      </div>
-      <input 
-        type="text" 
-        placeholder="Enter pet name" 
-        value={petName} 
-        onChange={(e) => setPetName(e.target.value)} 
-      />
-      <button onClick={handleCreatePet}>Create Pet</button>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-center mb-4">Pet Selection</h1>
+        <div className="shadow appearance-none border rounded w-1/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <div className="mb-6 ">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Choose a pet:
+              <select
+                value={species}
+                onChange={(e) => setSpecies(e.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              >
+                <option value="null"></option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="rat">Rat</option>
+              </select>
+            </label>
+          </div>
+          <input
+            type="text"
+            placeholder="Enter pet name"
+            value={petName}
+            onChange={(e) => setPetName(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          <button
+            onClick={handleCreatePet}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Create Pet
+          </button>
+        </div>
     </div>
   );
 };
